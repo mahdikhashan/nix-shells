e658cbf0e3c233b6971c49214b33d9f2d26cac87
mahdikhashan <mahdikhashan1@gmail.com>
2024-11-29 14:19:55 +0100

    add pyspark integration-tests requirements

--- lakeFS/shell.nix
+++ lakeFS/shell.nix
@@ -1,10 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-    buildInputs = [
-        pkgs.gnumake
-        pkgs.go
-        pkgs.nodejs_22
-        pkgs.maven
-        pkgs.zulu8
-    ];
+  buildInputs = [
+    pkgs.gnumake
+    pkgs.go
+    pkgs.nodejs_22
+    pkgs.maven
+    pkgs.zulu8
+    pkgs.python3Packages.virtualenv
+  ];
+
+  shellHook = ''
+    if [ ! -d "venv" ]; then
+      virtualenv venv
+    fi
+    source venv/bin/activate
+    pip install lakefs-sdk==1.43.0 python-on-whales==0.62.0 tenacity==8.2.2
+  '';
 }
